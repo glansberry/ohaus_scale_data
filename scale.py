6a8754bf718ece363b550ace2449f41bc1571187
Geoff Lansberry <geoff@kuvee.com>
2017-04-23 18:47:04 -0400

    improvements:
    use readable date and time
    auto create filename based on date and time
    print grams to screen
    add a bottle counter
    add test when intializing
    don't read unless there is something there

--- scale.py
+++ scale.py
@@ -4,14 +4,18 @@
 import serial
 import time
 #scale = serial.Serial('/dev/tty.usbserial-00002014', 9600, bytesize=8, stopbits=1, timeout=None)
+
+save_file = time.strftime("weigh_data%Y%m%d%H%M%S.csv", time.localtime())
 with serial.Serial() as scale:
     scale.baudrate = 9600
     scale.parity = serial.PARITY_NONE
     scale.bytesize = 8
     scale.stopbits = 1
-    scale.timeout = 10
+    scale.timeout = 1
     scale.port = '/dev/ttyUSB0'
     scale.open()
+    scale.reset_input_buffer()
+    print "Initializing Scale - please wait"
 
     time.sleep(0.1)
     scale.write(b'0P\r\n')  #auto print off
@@ -24,15 +28,21 @@ with serial.Serial() as scale:
     time.sleep(0.5)
     print scale.readline()
 
-    with open('weigh_data.csv', 'w+') as f:
+    print "Scale Initialized - appending to file: " + save_file
+    counter = 0;
+    with open(save_file, 'w+') as f:
         while True:
-            grams = scale.readline()
-            grams = grams.lstrip()
-            scale_time = time.time()
-            if grams.startswith(('0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '-')):
-                g = grams.split()
-                scale_out = str(str(scale_time) + ',' + g[0] + '\n')
-                print scale_out
-                f.write(scale_out)
-            f.close()
+            if scale.in_waiting > 3:
+                grams = scale.readline()
+                grams = grams.strip()
+                scale_time = time.time()
+                scale_time = time.strftime("%m/%d/%Y %H:%M:%S", time.localtime())
+                if grams.startswith(('0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '-')):
+                    counter += 1
+                    g = grams.split()
+                    scale_out = str(str(counter) + ',' + str(scale_time) + ',' + g[0] + '\n')
+                    f.write(scale_out)
+                    print str(counter) + ': ' + grams
+            else: time.sleep(0.1)
+        f.close()
     scale.close()
